5da5da6d40847dac17bb4b569974646772853497
Valtteri Valtanen <vade1997@gmail.com>
2020-03-05 15:13:35 +0200

    resize return button

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -3,11 +3,15 @@ import React from 'react';
 const Game = ({ game, setCurrent }) => {
   const text = {
     paddingLeft: 4,
-    fontSize: 13
+    fontSize: 13,
+  }
+  const button = {
+    height: '35px',
+    width: '105px'
   }
   return (
     <div>
-      <button onClick={()=>setCurrent(null)}>{'<<<'}</button>
+      <button style={button} onClick={()=>setCurrent(null)}>{'<<<'}</button>
       <h1>{game.name}</h1>
       <text style={text}>{game.text}</text>
     </div>
