a07e81d18d5b55edf75c560aa0d38c370952c377
Valtteri Valtanen <vade1997@gmail.com>
2020-03-05 14:40:38 +0200

    added a github link to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
   }
   const text = {
     paddingLeft: 3,
+    paddingTop: 3
   }
   return (
     <div style={style}>
@@ -14,6 +15,8 @@ const Footer = () => {
       <strong>----------------------------</strong>
       <br />
       <strong style={text}>tg: @vade123</strong>
+      <br />
+      <a style={text} href="https://github.com/vade123/game-archive">view on github</a>
     </div>
   )
 }
