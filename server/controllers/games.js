0d0b3e98939c68c612241c298d8ab8219493e5c6
Valtteri Valtanen <vade1997@gmail.com>
2020-03-08 13:50:48 +0200

    changed controller to use async/await

--- server/controllers/games.js
+++ server/controllers/games.js
@@ -1,24 +1,18 @@
 const gameRouter = require('express').Router();
 const Game = require('../models/game');
 
-gameRouter.get('/all', (req, res) => {
-  Game
-    .find()
-    .then((games) => {
-      res.json({ games });
-    });
+gameRouter.get('/all', async (req, res) => {
+  const games = await Game.find();
+  res.json({ games });
 });
 
-gameRouter.post('/add', (req, res) => {
+gameRouter.post('/add', async (req, res) => {
   const game = new Game({
     name: req.body.name,
     text: req.body.text,
   });
-  game
-    .save()
-    .then((result) => {
-      res.status(201).json({ name: result.name });
-    });
+  const savedGame = await game.save();
+  res.status(201).json({ name: savedGame.name });
 });
 
 module.exports = gameRouter;
