875a7b46ca177e3a49c06b99b6b87200b7d2848f
Valtteri Valtanen <vade1997@gmail.com>
2020-03-08 15:51:45 +0200

    store text with html tags

--- server/controllers/games.js
+++ server/controllers/games.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-
+const nl2br = require('nl2br');
 const gameRouter = require('express').Router();
 const Game = require('../models/game');
 const User = require('../models/user');
@@ -13,12 +13,13 @@ gameRouter.post('/add', async (req, res) => {
   const user = await User.findOne({ username: 'admin' });
   const passwordCorrect = await bcrypt.compare(req.body.password, user.passwordHash);
 
+  const text = ('<p>'.concat(req.body.text)).concat('</p>');
   if (!passwordCorrect) {
     res.status(401).json({ error: 'invalid password' });
   } else {
     const game = new Game({
       name: req.body.name,
-      text: req.body.text,
+      text: nl2br(text),
     });
     const savedGame = await game.save();
     res.status(201).json({ name: savedGame.name });
